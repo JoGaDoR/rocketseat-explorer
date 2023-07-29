602f82942264651b70e6663f2437eb3b519bd46b
JoGaDoR <thiagojogador171@gmail.com>
2023-07-29 15:42:12 -0300

    feat: refactory

--- Stage05/FASE02 - imc/01-calculadora-imc/02-refactory-to-module/js/script.js
+++ Stage05/FASE02 - imc/01-calculadora-imc/02-refactory-to-module/js/script.js
@@ -1,6 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
-import { notANumber, IMC } from './utils.js'
+import { notANumber, calculateIMC } from './utils.js'
 
 // variáveis - variables
 const form = document.querySelector('form')
@@ -13,16 +13,20 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
     
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if(showAlertError){
+    if(weightOrHeightIsNotANumber){
         AlertError.open
         return;
     }
 
     AlertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result){
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
