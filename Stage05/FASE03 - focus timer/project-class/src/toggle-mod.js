28b03a22e382ae9d553fc11e058bc36a2b67eb55
JoGaDoR <thiagojogador171@gmail.com>
2023-07-30 16:58:38 -0300

    feat: start js logic

---
+++ Stage05/FASE03 - focus timer/project-class/src/toggle-mod.js
@@ -0,0 +1,6 @@
+let darkMode = true
+const buttonToggle = document.getElementById("toggle-mode")
+
+buttonToggle.addEventListener('click', (event) => {
+    event.currentTarget.querySelector("span")
+})
