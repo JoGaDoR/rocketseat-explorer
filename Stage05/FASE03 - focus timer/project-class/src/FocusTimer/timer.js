13ca9dc41a7a9cf69129223c1926c06477a206db
JoGaDoR <thiagojogador171@gmail.com>
2023-07-31 20:02:52 -0300

    feat: countdown logic

--- Stage05/FASE03 - focus timer/project-class/src/FocusTimer/timer.js
+++ Stage05/FASE03 - focus timer/project-class/src/FocusTimer/timer.js
@@ -1,10 +1,31 @@
 import state from "./state.js"
 import * as el from "./elements.js"
+import { reset } from "./actions.js"
+
 
 export function countdonw(){
     if(!state.isRunning){
         return
     }
+
+    let minutes = Number(el.minutes.textContent)
+    let seconds = Number(el.seconds.textContent)
+
+    seconds--
+
+    if(seconds < 0){
+        seconds = 59
+        minutes--
+
+    }
+
+    if(minutes < 0){
+        reset()
+        return
+    }
+
+
+    updateDisplay(minutes, seconds)
 
     // RECURSÃO
     setTimeout(() => {
@@ -14,7 +35,7 @@ export function countdonw(){
 
 export function updateDisplay(minutes, seconds){
     // nullish coalesing operator
-    minute = minutes ?? state.minutes
+    minutes = minutes ??     state.minutes
     seconds = seconds ?? state.seconds
 
     el.minutes.textContent = String(minutes).padStart(2, "0")
