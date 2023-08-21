f2012d24f5801a8570fdded6cd912a8fc17bd232
JoGaDoR <thiagojogador171@gmail.com>
2023-08-21 15:01:53 -0300

    feat: js classes and inheritance

---
+++ Stage06/FASE02 - githubFavorites/aula4/js/Favorites.js
@@ -0,0 +1,31 @@
+// classe que vai conter a logica dos dados
+// como os dados serão estruturados
+
+export class Favorites{
+    constructor(root){
+        this.root = document.querySelector(root)
+
+        this.update()
+    }
+
+}
+
+
+// classe que vai criar a visualização e eventos do HTML
+export class FavoritesView extends Favorites{
+    constructor(root){
+        super(root)
+    }
+
+    update(){
+        this.removeAllTr()
+    }
+
+    removeAllTr(){
+        const tbody = this.root.querySelector("table tbody")
+
+        tbody.querySelectorAll("tr").forEach((tr) => {
+            tr.remove()
+        })
+    }
+}
