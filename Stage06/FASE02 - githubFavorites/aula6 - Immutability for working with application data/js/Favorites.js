3e87a4e4d4561c1e3c9d3e90be637255db82e2c8
JoGaDoR <thiagojogador171@gmail.com>
2023-08-22 20:35:43 -0300

    feat: remove button

--- Stage06/FASE02 - githubFavorites/aula6 - Immutability for working with application data/js/Favorites.js
+++ Stage06/FASE02 - githubFavorites/aula6 - Immutability for working with application data/js/Favorites.js
@@ -24,6 +24,12 @@ export class Favorites {
         }
       ]
   }
+
+  delete(user){
+    const filteredEntries = this.entries.filter((entry) => entry.login !== user.login)
+
+    this.entries = filteredEntries
+  }
 }
 
 // classe que vai criar a visualização e eventos do HTML
@@ -49,6 +55,16 @@ export class FavoritesView extends Favorites {
         row.querySelector(".repositories").textContent = user.public_repos
         row.querySelector(".followers").textContent = user.followers
 
+        row.querySelector(".remove").onclick = () => {
+          const isOk = confirm("Tem certeza que deseja deletar essa linha?")
+
+          if(isOk){
+            this.delete(user)
+          }
+        }
+        row.querySelector(".remove").addEventListener("click", (e) => {
+
+        })
 
         this.tbody.append(row)
     })
